e8dba9485735a7e3bb4421fe8f12182a58dcd56d
Kristina Pang <kvypang@gmail.com>
2022-04-05 21:06:14 -0700

    gameplay

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // start button - click (add eventListener)
-
 // timer that starts after clicking the start button
 // when the 1st question is answered, another question appears
 // if the question is answered incorrectly, time is taken from the clock
@@ -8,95 +7,117 @@
 //
 // when the game is over
 // then the user can enter their initials and score (saved)
-// scores and username saved on screen 
-
-//variables
-//timer 
-var counter = 10;
-let number = 0;
-var startBtn = document.querySelector('#start');
-
-startBtn.addEventListener("click",function(){
-    var myInterval = setInterval(function(){
-        var timer = document.querySelector("#timer");
-        timer.textContent = counter;
-        counter--;
-        if(counter==-1) {
-            clearInterval(myInterval)
-        }
-    },1000)
-    LoadQuestion(); //function call to load the first question
-});
+// scores and username saved on screen
 
 
-var questionText = document.getElementById("question-text")
-var currentQuestion = 0;
-var timePara = document.querySelector("#timePara");
-var timeLeft = 10;
 
-var optionA =  document.getElementById("btnA");
-var optionB =  document.getElementById("btnB");
-var optionC =  document.getElementById("btnC");
-var optionD =  document.getElementById("btnD");
-
-
-const LoadQuestion = ()=>{
-    let display = document.getElementById("quizQuestions");
-    let quest = document.createElement("div")
-   quest.innerHTML = questions[number].question; 
-    display.appendChild(quest);
-//    let opt = "";
-   for (let i = 0; i<questions[number].options.length; i++) {
-     let item = document.createElement("div");
-     item.id = "option" + i; 
-     item.innerHTML = questions[number].options[i];
-     display.appendChild(item);
-     document.getElementById("option" + i).addEventListener("click", function(){
-         CheckResponse("option" + i);
-     }
-     )
-   }
-}
-
-const CheckResponse = (id)=>{
-
-}
-
-//event listeners 
-// // gameStart.addEventListener("click", )
-// optionA.addEventListener("click", chooseA);
-// optionB.addEventListener("click", chooseB);
-// optionC.addEventListener("click", chooseC);
-// optionD.addEventListener("click", chooseD);
-
-
-//quiz questions
 var questions = [
-{   question: "What is the description for this arithmetic operator: %",
+  {
+    question: "What is the description for this arithmetic operator: %",
     options: ["A. Oculus", "B. Expelliarmus", "C. Modulus", "4. Imperio"],
-    answer: "C. Modulus"
-},
-{   question: "Which is not a way to declare a variable?",
+    answer: "C. Modulus",
+  },
+  {
+    question: "Which is not a way to declare a variable?",
     options: ["A. const", "B. dec", "C. var", "4. let"],
-    answer: "B. dec"
-}, 
-{   question: "What function can you use find out if a variable is true?",
-    options: ["A. Ganglion()", "B. TrueFalse()", "C. Bullion()", "D. Boolean()"],
-    answer: "D. Boolean"
-}, 
-{   question: "How can you add a comment in a JavaScript file?",
-    options: ["A. --This is a comment", "B. <!--This is a comment-->", "C. ~*This is a comment", "D. //This is a comment"],
-    answer: "D. //This is a comment"
-}, 
-{   
+    answer: "B. dec",
+  },
+  {
+    question: "What function can you use find out if a variable is true?",
+    options: [
+      "A. Ganglion()",
+      "B. TrueFalse()",
+      "C. Bullion()",
+      "D. Boolean()",
+    ],
+    answer: "D. Boolean()",
+  },
+  {
+    question: "How can you add a comment in a JavaScript file?",
+    options: [
+      "A. --This is a comment",
+      "B. <!--This is a comment-->",
+      "C. ~*This is a comment",
+      "D. //This is a comment",
+    ],
+    answer: "D. //This is a comment",
+  },
+  {
     question: "How do you create a function in JavaScript?",
-    options:  ["A. createMyFunction()", "B. function.myFunction()", "C. function myFunction() ", "D. function = myFunction()"],
-    answer: "C. function myFunction()"
-}
+    options: [
+      "A. createMyFunction()",
+      "B. function.myFunction()",
+      "C. function myFunction()",
+      "D. function = myFunction()",
+    ],
+    answer: "C. function myFunction()",
+  },
 ];
 
+var counter = 75;
+let number = 0;
+var startBtn = document.querySelector("#start");
 
-// for (let index = 0; index < array.length; index++) {
-//     const element = array[index];
-    
-// }
+var myInterval;
+
+startBtn.addEventListener("click", function () {
+  myInterval = setInterval(function () {
+    var timer = document.querySelector("#timer");
+    timer.textContent = counter;
+    counter--;
+    if (counter == -1) {
+      endGame();
+    }
+  }, 1000);
+  LoadQuestion(); //function call to load the first question
+});
+
+var questionText = document.getElementById("question-text");
+var timePara = document.querySelector("#timePara");
+
+const LoadQuestion = () => {
+  let display = document.getElementById("quizQuestions");
+  display.innerHTML = "";
+  let quest = document.createElement("div");
+  quest.innerHTML = questions[number].question;
+  display.appendChild(quest);
+  //    let opt = "";
+  for (let i = 0; i < questions[number].options.length; i++) {
+    let item = document.createElement("div");
+    item.id = "option" + i;
+    item.innerHTML = questions[number].options[i];
+    display.appendChild(item);
+    document
+      .getElementById("option" + i)
+      .addEventListener("click", function (event) {
+        CheckResponse("option" + i, event);
+      });
+  }
+};
+
+const CheckResponse = (id, event) => {
+  console.log(event);
+  if (event.target.textContent === questions[number].answer) {
+    console.log(true);
+  } else {
+    console.log(false);
+    counter = counter - 10;
+  }
+  number++;
+  if (number <= questions.length - 1) {
+    LoadQuestion();
+  } else {
+    endGame();
+  }
+};
+
+function endGame() {
+  clearInterval(myInterval);
+  console.log(counter);
+  let display = document.getElementById("quizQuestions");
+  display.innerHTML = "";
+  var timer = document.querySelector("#timer");
+  timer.textContent = counter;
+}
+
+// user initials, score, local storage
